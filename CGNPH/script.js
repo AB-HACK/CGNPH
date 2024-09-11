7180ebb897fc4df65cd746b6ada9f3c3c2ceb563
AB-TECH <abdullahimoradeyo13@gmail.com>
2024-09-11 19:23:41 +0100

    changes

--- CGNPH/script.js
+++ CGNPH/script.js
@@ -41,3 +41,28 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+ // Get the modal element
+ const modal = document.getElementById("modal");
+
+ // Get the button that opens the modal
+ const readMoreBtn = document.getElementById("readMoreBtn");
+
+ // Get the <span> element that closes the modal
+ const closeBtn = document.querySelector(".close-btn");
+
+ // When the user clicks the button, open the modal
+ readMoreBtn.onclick = function () {
+     modal.style.display = "block";
+ }
+
+ // When the user clicks on <span> (x), close the modal
+ closeBtn.onclick = function () {
+     modal.style.display = "none";
+ }
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function (event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
