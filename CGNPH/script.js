8fa7d1df6d30c7fc29e76b45e3c967eae25d5c8d
AB-TECH <abdullahimoradeyo13@gmail.com>
2024-09-24 19:28:36 +0100

    content adjustment

--- CGNPH/script.js
+++ CGNPH/script.js
@@ -76,21 +76,35 @@ window.onclick = function (event) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.read-more-btn').forEach(button => {
-      button.addEventListener('click', () => {
-        const moreText = button.previousElementSibling.querySelector('.more-text');
-  
-        // Toggle the visibility of the more text
-        if (moreText.style.display === 'inline') {
-          moreText.style.display = 'none';
-          button.textContent = 'Read More';
-        } else {
-          moreText.style.display = 'inline';
-          button.textContent = 'Read Less';
-        }
-      });
+        button.addEventListener('click', () => {
+            const moreText = button.previousElementSibling.querySelector('.more-text');
+
+            // Toggle the visibility of the more text
+            if (moreText.style.display === 'inline') {
+                moreText.style.display = 'none';
+                button.textContent = 'Read More';
+            } else {
+                moreText.style.display = 'inline';
+                button.textContent = 'Read Less';
+            }
+        });
     });
-  });
-  
+});
+
+
+
+function toggleContent() {
+    var content = document.getElementById("folktale-content");
+    var button = document.querySelector(".toggle-button");
+    if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden");
+        button.textContent = "Show Less";
+    } else {
+        content.classList.add("hidden");
+        button.textContent = "Show More";
+    }
+}
+
 
